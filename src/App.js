7060677724c5e6d4a277d2850e1b9654c9c24fd3
Ganesh651 <ganireddy651@gmail.com>
2024-01-05 01:36:13 +0530

    functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { Routes, Route } from "react-router-dom";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import FormContext from './context/FormContext';
 
-function App() {
+
+const App = () => {
+  const [projectName, setProjectName] = useState("")
+  const [projectLink, setProjectLink] = useState("")
+  const [description, setDescription] = useState("")
+  const [projectsList, setProjectList] = useState([])
+  console.log(projectsList)
+
+  const changeDescription = (value) => {
+    setDescription(value)
+  }
+
+  const changeProjectLink = (value) => {
+    setProjectLink(value)
+  }
+
+  const changeProjectName = (value) => {
+    setProjectName(value)
+  }
+
+  const submitForm = () => {
+    const newProject = {
+      id: uuidv4(),
+      projectName,
+      projectLink,
+      description
+    }
+    setProjectList(prevState => [...prevState, newProject])
+    setDescription("")
+    setProjectLink("")
+    setProjectName("")
+    console.log("project added from the form")
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <FormContext.Provider value={{
+      changeDescription,
+      changeProjectLink,
+      changeProjectName,
+      submitForm,
+      projectName,
+      projectLink,
+      projectsList,
+      description
+    }}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Footer />} />
+        <Route path='/contact' element={< Footer />} />
+      </Routes>
+    </FormContext.Provider >
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './index.css'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className='navbar-container'>
+      <span className='website-logo'>
+        Madelyn Torff
+      </span>
+      <ul className='nav-items-container'>
+        <li className='nav-item'>
+          <NavLink to="/" className="link">
+            About
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to="/projects" className="link">
+            Projects
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to="/contact" className="link">
+            Contacts
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
